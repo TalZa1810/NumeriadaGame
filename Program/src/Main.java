98edea04dc467709025f47e2e7ff1b7ffd35b245
TalZa <tal.zaidman1810@gmail.com>
2017-01-01 17:00:18 +0200

    javaFX GameManagerFX, changing Main method and creating a controller

--- Program/src/Main.java
+++ Program/src/Main.java
@@ -1,11 +1,23 @@
-import manager.GameManager;
+import javafx.application.Application;
+
+import javafx.stage.Stage;
+import manager.GameManagerFX;
 
 import static java.lang.System.exit;
 
-public class Main {
-    public static void main(String args[]){
-        GameManager manager = new GameManager();
+public class Main extends Application {
+
+    public static void main(String args[]) throws Exception{
+
+        GameManagerFX manager = new GameManagerFX();
+        launch(args);
         manager.run();
         exit(0);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+
     }
 }
