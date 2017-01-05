9b56b8068de959feaaf159f8345a235bded33452
TalZa <tal.zaidman1810@gmail.com>
2017-01-05 14:16:23 +0200

    binding

--- GameUI/src/javafx_ui/gamePane/GameController.java
+++ GameUI/src/javafx_ui/gamePane/GameController.java
@@ -1,5 +1,7 @@
 package javafx_ui.gamePane;
 
+import javafx.beans.binding.Bindings;
+import javafx.beans.property.SimpleBooleanProperty;
 import javafx.beans.property.SimpleIntegerProperty;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.fxml.FXML;
@@ -11,12 +13,14 @@ import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 import javafx_ui.boardPane.BoardController;
 import javafx_ui.loaderPane.LoaderController;
 import javafx_ui.playersPane.PlayersController;
 import javafx_ui.statusPane.StatusController;
 
+import java.io.File;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -47,9 +51,12 @@ public class GameController implements Initializable{
     private PlayersController m_Players;
     private Stage m_PrimaryStage;
 
+
     private SimpleIntegerProperty[] m_PlayersID;
     private SimpleStringProperty[] m_PlayersNames;
     private SimpleIntegerProperty[] m_PlayersScore;
+    private SimpleStringProperty m_FilePath = new SimpleStringProperty();
+    private SimpleBooleanProperty m_isFileSelected = new SimpleBooleanProperty();
 
     public GameController(){
         m_MainWindow = new BorderPane();
@@ -62,11 +69,26 @@ public class GameController implements Initializable{
     @Override
     public void initialize(URL location, ResourceBundle resources) {
 
+        pathTextBox.textProperty().bind(Bindings.format("%s", m_FilePath ));
+        loadButton.disableProperty().bind(m_isFileSelected.not());
+
+
     }
 
     @FXML
     public void browseButtonClicked(){
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Select XML File");
+        fileChooser.setInitialDirectory(new File("C:\\"));
+        fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("*.xml"));
+        File selectedFile = fileChooser.showOpenDialog(m_PrimaryStage);
+        if (selectedFile ==  null){
+            return;
+        }
 
+        String path = selectedFile.getAbsolutePath();
+        m_FilePath.set(path);
+        m_isFileSelected.set(true); //toggle
     }
 
     @FXML
