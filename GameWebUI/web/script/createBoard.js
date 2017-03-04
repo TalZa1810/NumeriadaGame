1f4c71d2f3918682e4b4809be3d88358e71e7797
TalZa <tal.zaidman1810@gmail.com>
2017-03-04 15:10:52 +0200

    display board buttons when clicking "show board" button

--- GameWebUI/web/script/createBoard.js
+++ GameWebUI/web/script/createBoard.js
@@ -1,12 +1,6 @@
-/**
- * Created by nicom on 10/28/2016.
- */
 
+function getShowBoard(table){
 
-
-
-function getShowBoard(table)
-{
     var actionType = "getShowBoard";
     var gamettl = $("#gameTable").find("tr.success").attr('value');
 
@@ -44,32 +38,53 @@ function getBoard(table) {
 function createBoard(theBoard,table) {
     console.log("board create");
     table.addClass("myTable");
-    createTopPart(theBoard, table);
-    createDownPart(theBoard, table);
+    createBoard(theBoard, table);
+    //createDownPart(theBoard, table);
 }
 
 
-function createTopPart(board, table) {
+function createBoard(board, table) {
     var columns = board.m_BoardSize;
     var rows = board.m_BoardSize;
 
     for (var row = 0; row < rows; row++) {
+
+        //A <tr> element contains one or more <th> or <td> elements.
         var tr = document.createElement('tr');
         for (var column = 0; column < columns; column++) {
+
+            //Standard cells - contains data (created with the <td> element)
             var td = document.createElement('td');
-            td.setAttribute("column",column- maxRowBlock);
-            td.classList.add("block");
-            if (column >= maxRowBlock && maxColumnBlock - row <= columnsBlocks[column - maxRowBlock].length) {
-                var index =columnsBlocks[column - maxRowBlock].length - maxColumnBlock + row;
-                td.setAttribute("blockIndex",index);
-                td.innerText = columnsBlocks[column - maxRowBlock][index].m_Value;
-            }
+            td.setAttribute("column",column);
+            td.setAttribute("row",row);
+
+            var btn = document.createElement("button");
+            btn.setAttribute("column",column);
+            btn.setAttribute("row",row);
+            //btn.setAttribute("width", 32 );
+            //btn.setAttribute("height",32 );
+            //btn.classList("boardBtn");
+
+
+            td.appendChild(btn);
+
+
+            //td.classList.add("block");
+
+            /*
+             if (column >= maxRowBlock && maxColumnBlock - row <= columnsBlocks[column - maxRowBlock].length) {
+             var index =columnsBlocks[column - maxRowBlock].length - maxColumnBlock + row;
+             td.setAttribute("blockIndex",index);
+             td.innerText = columnsBlocks[column - maxRowBlock][index].m_Value;
+             }*/
+
             tr.appendChild(td);
         }
         table.append(tr);
     }
 }
 
+/*
 function createDownPart(board, table) {
     var theBoard = board.board;
     var rowsBlocks = board.rowsBlocks;
@@ -103,6 +118,8 @@ function createDownPart(board, table) {
         table.append(tr);
     }
 }
+
+*/
 
 function getMaxLengthOfList(list) {
     var retValue = 0;
